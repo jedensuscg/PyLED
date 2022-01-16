9038ae1d93803dab75724cdedbfdd0a9e0296bdd
James Edens <jedensuscg@gmail.com>
2022-01-15 21:17:54 -0500

    Running led_layout triggers led_calc Added more documentation Minor output formatting. updated README

--- README.md
+++ README.md
@@ -5,9 +5,54 @@ Simple little tool I am making for help in setting up simple LED circuits. There
 Currently is supports (though there is no menu yet, just manually running the functions or using the Jupyter notebook layout)
 - Create a basic ASCII (I mean basic) layout of a circuit given parameters such as power source voltage, number of LED's, forward voltage of LED's, and current draw of LED's. Currently all LED's have to be identical.
 - It can spit out estimated total current draw on the power source. If using a boost converter, it includes the draw from this as well, determined by the given input > output voltage of the boost converter. This is also used to give an estimated battery life given the battery mAh input, regardless of boost converter use.
-- These two functions are not tied together. So you have to run them independently.
+- Total current draw can be run by itself, with the proper inputs. Running the led_layout will also run the led_calc to give current draw and battery run time.
 
-To run the LED Schematic, use led_layout with the required inputs. uses DOCSTRING to help.
-To run the Total Current draw use the led_cal function with required inputs. isBoosted = False will ignore any boost converter input and isBoosted = True will include it.
+Each of the utility functions can also be used if you don't need to run a full calculation:
 
-Next up will be to pass the LED draw of the LED's directly into the battery run time calc. For now, you can just multiply the number of parallel circuits spit out by the schematic by the current draw of the LED is use. so 3 parallel circuits of 20mA LED's will draw 60 mA. Input this into the led_calc for the battery run time. 
+- get_battery_life(battery_capacity,total_load_current): returns the run time of a battery given its mAh rating and the current draw of the circuit. Uncomment the print line to get results.
+- get_current_limiter(volts_in,led_forward_volts, max_leds_per_wire,led_current): Returns the best current limiter for a given circuit branch. Includes the average 5% tolerance of most resistors. Selects the next highest standard resistor if calculated resistance is not a standard resistor. Uncomment print to see result.
+
+### Terminal output examples
+```
+KEY:  
+# = LED
++ = ANODE(Positive leg)
+[82Ω] = Calculated Resistor
+```  
+4.5v input source, 6 led's with forward voltage of 3v and current of 20mA. using a Boost converter with a rated max efficiency of 90%, and a desired output voltage of 18v.
+Ran with ```led_layout(4.5,3,6,.02,(True,18,.9),2300,)```
+```
+Max LED's per parallel line: 6 LED's
+Total parallel wires with max LED's: 1 parallel wires
+18V-+-#--#--#--#--#--#-[1Ω]-
+END OF CIRCUIT
+
+BATTERY RUN TIMES AND CURRENT DRAWS
+********
+***Boost converter efficiency of 90.0%***
+Battery Run Time = 25hours and 33 minutes.
+Total current draw including boost converter: 90 mA / 0.09 Amps
+
+*\**Boost converter efficiency of 82.0%***
+Battery Run Time = 23hours and 0 minutes.
+Total current draw including boost converter: 100 mA / 0.1 Amps
+
+*\**Boost converter efficiency of 50.0%***
+Battery Run Time = 14hours and 22 minutes.
+Total current draw including boost converter: 160 mA / 0.16 Amps
+```
+Same input as above, except use boost converter set to **FALSE** this ignoring it.
+```Max LED's per parallel line: 1 LED's
+Total parallel wires with max LED's: 6 parallel wires
+4.5V-+-#-[82Ω]-
+    -+-#-[82Ω]-
+    -+-#-[82Ω]-
+    -+-#-[82Ω]-
+    -+-#-[82Ω]-
+    -+-#-[82Ω]-
+END OF CIRCUIT
+
+BATTERY RUN TIMES AND CURRENT DRAWS
+********
+Battery Run Time = 19hours and 9 minutes.
+Total current draw: 120 mA / 0.12 Amps```
--- led_calcs.py
+++ led_calcs.py
@@ -4,30 +4,24 @@ import numpy as np
 import bisect
 from numpy.core.multiarray import array, empty
 from numpy.lib.arraysetops import setdiff1d
-#Power(Watts) = Volts(V) * Amps(A)
-led_voltages = np.array([3,3,3,2.4,2.4])
-eff_list = [.94, .82, .7, .5]
+
+eff_list = [.94, .82, 0.5]
 standard_resistors = [1,10,100,1000,10000,100000,1000000,1.1,11,110,1100,11000,110000,1100000,1.2,12,120,1200,12000,120000,1200000,1.3,13,130,1300,13000,130000,1300000,1.5,15,150,1500,15000,150000,1500000,1.6,16,160,1600,16000,160000,1600000,1.8,18,180,1800,18000,180000,1800000,2,20,200,2000,20000,200000,2000000,2.2,22,220,2200,22000,220000,2200000,2.4,24,240,2400,24000,240000,2400000,2.7,27,270,2700,27000,270000,2700000,3,30,300,3000,30000,300000,3000000,3.3,33,330,3300,33000,330000,3300000,3.6,36,360,3600,36000,360000,3600000,3.9,39,390,3900,39000,390000,3900000,4.3,43,430,4300,43000,430000,4300000,4.7,47,470,4700,47000,470000,4700000,5.1,51,510,5100,51000,510000,5100000,5.6,56,560,5600,56000,560000,5600000,6.2,62,620,6200,62000,620000,6200000,6.8,68,680,6800,68000,680000,6800000,7.5,75,750,7500,75000,750000,7500000,8.2,82,820,8200,82000,820000,8200000,9.1,91,910,9100,91000,910000,9100000]
 sorted_resistors = sorted(standard_resistors, key = lambda x:float(x))
+
+
 def boost_current_draw(volts_in: int,volts_out: Union[int,tuple,list],load_current: Union[int, float],max_efficiency: float):
-#region DOCSTRING
     """The current draw of circuit accounting for boost converter draw
-    Parameters
-    ----------
-    volts_in : int
-        Voltage from the original power source
-    volts_out : int or list of int or tuple of in
-        Voltage required from boost converter. Total voltage if know or list of led's forward voltages.
-    load_current : float or int
-        The calculated current of the load
-    max_efficiency : float
-        The listed maximum efficiency of the boost converter
-    Returns
-    -------
-    list
-        [Total current in milliAmps, Total current in Amps]
+
+    Args:
+        volts_in (int): Voltage from the original power source
+        volts_out (Union[int,tuple,list]): Voltage required from boost converter. Total voltage if know or list of led's forward voltages.
+        load_current (Union[int, float]): The calculated current of the load
+        max_efficiency (float): The listed maximum efficiency of the boost converter
+
+    Returns:
+        [list]: [Total current in milliAmps, Total current in Amps]
     """
-#end region
     if type(volts_out) is np.ndarray:
         volts_out = sum(volts_out)
     total_current_amps = round(load_current*volts_out/(volts_in*max_efficiency),2)
@@ -35,37 +29,45 @@ def boost_current_draw(volts_in: int,volts_out: Union[int,tuple,list],load_curre
     #print(f"Current output of boost converter: {total_current_ma} mA")
     return [total_current_ma, total_current_amps]
 
+def get_battery_life(battery_capacity, total_load_current: float):
+    """[summary]
 
-def Battery_Life(battery_capacity, total_load_current):
+    Args:
+        battery_capacity (int): Rated battery capacity in mAh
+        total_load_current (float): Total current draw in Amps
+    """
     battery_life_list = math.modf(battery_capacity / total_load_current)
     battery_life_whole_hour = int(battery_life_list[1])
     battery_life_minutes = math.floor(battery_life_list[0] * 60)
     return[battery_life_whole_hour,battery_life_minutes]
     # print(f"Battery Run Time = {battery_life_whole_hour}hours and {battery_life_minutes} minutes")
 
+def led_layout(volts_in: Union[int, float], led_forward_volts: Union[int, float], led_count: int, led_current: float, boost_settings: tuple = (True,9,.94), battery_mah: int = 2500):
+    """Prints an ASCII layout of an LED circuit with given parameters. Chains to led_calc()
 
-def led_layout(volts_in: Union[int, float], led_forward_volts: Union[int, float], led_count: int, led_current: float):
-#region DOCSTRING
-    """Prints an ASCII layout of an LED circuit with given parameters
-    ----------
-    volts_in : int or float
-        Voltage from the original power source or boost converter
-    led_forward_volts : int or float
-        Rated forward voltage of the LED's being used.
-    led_count : int
-        number of LED's being used.
-    led_current : float
-        The rated current draw of the LED, in AMPS (20 mA = .02 Amps)
+    Args:
+        volts_in (Union[int, float]): Voltage from the original power source or boost converter
+        led_forward_volts (Union[int, float]): Rated forward voltage of the LED's being used.
+        led_count (int): number of LED's being used.
+        led_current (float): The rated current draw of the LED, in AMPS (20 mA = .02 Amps)
+        boost_settings (tuple, optional): Settings for boost converter. (isBoosted,desired boost voltage,boost coverter max efficiency) Defaults to (True,9,.94).
+        battery_mah (int, optional): Rated mAh of battery source. Defaults to 2500.
     """
-#end region
+
+    if boost_settings[0]:
+        actual_volts_in = boost_settings[1]
+    else:
+        actual_volts_in = volts_in
     led = "-#-"
-    total_forward_volts = led_forward_volts * led_count
-    led_per_wire = divmod(volts_in,led_forward_volts)
+    total_LED_current = 0
+    led_per_wire = divmod(actual_volts_in,led_forward_volts)
     total_wires = divmod(led_count,led_per_wire[0])
     
     max_leds_per_wire = int(led_per_wire[0])
     full_wires_count = int(total_wires[0])
     remaining_leds = int(total_wires[1])
+
+    
 
     print(f"Max LED's per parallel line: {max_leds_per_wire} LED's")
     print(f"Total parallel wires with max LED's: {full_wires_count} parallel wires")
@@ -73,64 +75,76 @@ def led_layout(volts_in: Union[int, float], led_forward_volts: Union[int, float]
         print(f"wire with any remaining LED's: {remaining_leds}")
 
     for x in range(0, full_wires_count):
-        resistor = get_current_limiter(volts_in,led_forward_volts,max_leds_per_wire,led_current)
+        total_LED_current += led_current
+        resistor = get_current_limiter(actual_volts_in,led_forward_volts,max_leds_per_wire,led_current)
         if x == 0:
-            print(f"{volts_in}V-+" + led * max_leds_per_wire + f"[{resistor}\u03A9]-")
+            print(f"{actual_volts_in}V-+" + led * max_leds_per_wire + f"[{resistor}\u03A9]-")
         else:
-            print(' '*len(str(volts_in)) + " " + led * max_leds_per_wire + f"[{resistor}\u03A9]-")
+            print(' '*len(str(actual_volts_in)) + " -+" + led * max_leds_per_wire + f"[{resistor}\u03A9]-")
     
-    if remaining_leds > 0: 
-        resistor_remain = get_current_limiter(volts_in,led_forward_volts,remaining_leds,led_current)
-        print(' '*len(str(volts_in)) + " " + led * remaining_leds + f"[{resistor_remain}\u03A9]-")
+    if remaining_leds > 0:
+        total_LED_current += led_current
+        resistor_remain = get_current_limiter(actual_volts_in,led_forward_volts,remaining_leds,led_current)
+        print(' '*len(str(actual_volts_in)) + " +" + led * remaining_leds + f"[{resistor_remain}\u03A9]-")
+    print("END OF CIRCUIT\n")
 
-def get_current_limiter(volts_in,led_forward_volts, max_leds_per_wire,led_current):
+    led_calc(volts_in,boost_settings[1],boost_settings[2],battery_mah,load_amp = total_LED_current, isBoosted = boost_settings[0])
+
+def get_current_limiter(volts_in:float,led_forward_volts:float, max_leds_per_wire:int,led_current:float):
+    """Gets the best current limiting resistor for a given line in the LED circuit, based on a standard list of resistors.
+
+    Args:
+        volts_in (float): The input voltage from power source.
+        led_forward_volts (float): Rated forward voltage of the LED's in this wire.
+        max_leds_per_wire (int): Number of LED's that will be on this wire.
+        led_current (float): The rated current draw of the LED's used.
+
+    Returns:
+        [int]: The calculated resistor to use.
+    """
     resistor = math.ceil((volts_in - (led_forward_volts * max_leds_per_wire)) / led_current)
     if resistor == 0:
-        return 1
+        calculated_resistor = 1
     else:
+        resistor = resistor * 1.05
         if not resistor in sorted_resistors:
             next_highest = sorted_resistors[bisect.bisect(sorted_resistors, resistor)]
-            return next_highest
+            calculated_resistor = next_highest
         else:
-            return resistor
+            calculated_resistor = resistor
+
+    #print(f"Calculated resistor should be:{calculated_resistor}\u03A9")
+    return calculated_resistor
 
 def led_calc(volts_in: Union[int,float], volts_out:Union[int,float], max_efficiency: float, battery_capacity: int, load_amp: float = .02, load_ma: float = 0, isBoosted: bool = False,):
-    #region DOCSTRING
     """Outputs total current draw from power source, including that from boost converter. Gives battery run times.
-    ----------
-    volts_in : int or float
-        Voltage from the original power source or boost converter
-    volts_out : int or float
-        Desired voltage from boost converter
-    max_efficiency : float
-        Max  rated efficiency of boost converter
-    battery_capacity : int
-        Rated capacity of battery in mAh
-    load_amp: float
-        Current draw of the LED's, in Amps. Default .02 Optional
-    load_ma: float
-        Current Current draw of the LED's, in milliAmps. Default 0. Any other numbers will override load_amp setting.
-    usBoosted: bool
-        Sets if the circuit is using a boost converter, or straight draw from the power source(i.e battery)
+    Args:
+        volts_in (Union[int,float]): Voltage from the original power source or boost converter  
+        volts_out (Union[int,float]): Desired voltage from boost converter
+        max_efficiency (float): Max  rated efficiency of boost converter
+        battery_capacity (int): Rated capacity of battery in mAh
+        load_amp (float, optional): Current draw of the LED's, in Amps. Default .02 Optional
+        load_ma (float, optional): Current Current draw of the LED's, in milliAmps. Default 0. Any other numbers will override load_amp setting.
+        isBoosted (bool, optional): Sets if the circuit is using a boost converter, or straight draw from the power source(i.e battery)
     """
-#end region    
+
     eff_list[0] = max_efficiency
     battery_life_list = []
     length = len(eff_list)
+    print("BATTERY RUN TIMES AND CURRENT DRAWS\n********")
     if load_ma != 0:
         load_amp = load_ma / 1000
         print("Ignoring load_amp value(is entered) as mA value was input")
     if isBoosted:
-
         for i in range(length):
             total_load_current = boost_current_draw(volts_in,volts_out,load_amp, eff_list[i])
             eff_load_ma = total_load_current[0]
             eff_load_amp = total_load_current[1]
-            battery_life = Battery_Life(battery_capacity,eff_load_ma)
+            battery_life = get_battery_life(battery_capacity,eff_load_ma)
             battery_life_list.append([eff_load_ma,eff_load_amp,battery_life])
         print_current_msg = "Total current draw including boost converter:"
     else:
-        battery_life_list.append(Battery_Life(battery_capacity,load_amp * 1000))
+        battery_life_list.append(get_battery_life(battery_capacity,load_amp * 1000))
         print_current_msg = "Total current draw:"
 
     if len(battery_life_list) > 1:
@@ -140,7 +154,6 @@ def led_calc(volts_in: Union[int,float], volts_out:Union[int,float], max_efficie
             print(f"{print_current_msg} {battery_life_list[x][0]} mA / {battery_life_list[x][1]} Amps\n")
     else:
         print(f"Battery Run Time = {battery_life_list[0][0]}hours and {battery_life_list[0][1]} minutes.")
-        print(f"{print_current_msg} {load_amp * 1000} mA / {load_amp} Amps")
-
-led_layout(16,3,8,.02)
-led_calc(4.5,18,.94,750,load_ma=20,isBoosted=True)
+        print(f"{print_current_msg} {int(load_amp * 1000)} mA / {round(load_amp,3)} Amps")
+    
+led_layout(4.5,3,6,.02,(False,18,.9),2300,)
